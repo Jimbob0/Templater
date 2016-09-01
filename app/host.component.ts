6ce9075571a4ad34c9ba4b463f616a5212613d5a
Jimbob0 <jneal3@citadel.edu>
2016-09-01 05:09:30 -0600

    commit

--- app/host.component.ts
+++ app/host.component.ts
@@ -14,36 +14,18 @@ import { HostService } from './host.service';
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    <input
-                        class="form-control"
-                        type="text"
-                        placeholder="Main Title"
-                        [ngModel]="input.title"
-                        (keyup)="onTitleChange($event)"
-                    />
+                    {{service.items.title}}
                 </h1>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Box 1 Title"
-                                [ngModel]="input.b1title"
-                                (keyup)="onb1titleChange($event)"
-                            />
+                            {{service.items.b1title}}
                         </h4>
                     </div>
                     <div class="panel-body">
-                        <textarea
-                            class="form-control"
-                            rows="6"
-                            placeholder="Box 1 Contents"
-                            [ngModel]="input.b1contents"
-                            (keyup)="onb1contentsChange($event)"
-                        ></textarea>
+                        {{service.items.b1contents}}
                     </div>
                 </div>
             </div>
@@ -51,23 +33,11 @@ import { HostService } from './host.service';
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Box 2 Title"
-                                [ngModel]="input.b2title"
-                                (keyup)="onb2titleChange($event)"
-                            />
+                            {{service.items.b2title}}
                         </h4>
                     </div>
                     <div class="panel-body">
-                        <textarea
-                            class="form-control"
-                            rows="6"
-                            placeholder="Box 2 Contents"
-                            [ngModel]="input.b2contents"
-                            (keyup)="onb2contentsChange($event)"
-                        ></textarea>
+                        {{service.items.b2contents}}
                     </div>
                 </div>
             </div>
@@ -75,23 +45,11 @@ import { HostService } from './host.service';
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Box 3 Title"
-                                [ngModel]="input.b3title"
-                                (keyup)="onb3titleChange($event)"
-                            />
+                            {{service.items.b3title}}
                         </h4>
                     </div>
                     <div class="panel-body">
-                        <textarea
-                            class="form-control"
-                            rows="6"
-                            placeholder="Box 3 Contents"
-                            [ngModel]="input.b3contents"
-                            (keyup)="onb3contentsChange($event)"
-                        ></textarea>
+                        {{service.items[0].b3contents}}
                     </div>
                 </div>
             </div>
@@ -102,127 +60,37 @@ import { HostService } from './host.service';
         <div class="row">
             <div class="col-lg-12">
                 <h2 class="page-header">
-                    <input
-                        class="form-control"
-                        type="text"
-                        placeholder="Picture Title"
-                        [ngModel]="input.ptitle"
-                        (keyup)="onptitleChange($event)"
-                    />
+                {{service.items.ptitle}}
                 </h2>
             </div>
             <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Picture source URL"
-                            [ngModel]="input.p1Source"
-                            (keyup)="onp1SourceChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.p1Link"
-                            (keyup)="onp1LinkChange($event)"
-                        />
+                <a href="{{service.items.p1Link}}">
+                    <img class="img-responsive img-portfolio img-hover" src="{{service.items.p1Source}}" alt="">
                 </a>
             </div>
             <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Picture source URL"
-                            [ngModel]="input.p2Source"
-                            (keyup)="onp2SourceChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.p2Link"
-                            (keyup)="onp2LinkChange($event)"
-                        />
+                <a href="{{service.items.p2Link}}">
+                    <img class="img-responsive img-portfolio img-hover" src="{{service.items.p2Source}}" alt="">
                 </a>
             </div>
             <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Picture source URL"
-                            [ngModel]="input.p3Source"
-                            (keyup)="onp3SourceChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.p3Link"
-                            (keyup)="onp3LinkChange($event)"
-                        />
+                <a href="{{service.items.p3Link}}">
+                    <img class="img-responsive img-portfolio img-hover" src="{{service.items.p3Source}}" alt="">
                 </a>
             </div>
             <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Picture source URL"
-                            [ngModel]="input.p4Source"
-                            (keyup)="onp4SourceChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.p4Link"
-                            (keyup)="onp4LinkChange($event)"
-                        />
+                <a href="{{service.items.p4Link}}">
+                    <img class="img-responsive img-portfolio img-hover" src="{{service.items.p4Source}}" alt="">
                 </a>
             </div>
             <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Picture source URL"
-                            [ngModel]="input.p5Source"
-                            (keyup)="onp5SourceChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.p5Link"
-                            (keyup)="onp5LinkChange($event)"
-                        />
+                <a href="{{service.items.p5Link}}">
+                    <img class="img-responsive img-portfolio img-hover" src="{{service.items.p5Source}}" alt="">
                 </a>
             </div>
             <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Picture source URL"
-                            [ngModel]="input.p6Source"
-                            (keyup)="onp6SourceChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.p6Link"
-                            (keyup)="onp6LinkChange($event)"
-                        />
+                <a href="{{service.items.p6Link}}">
+                    <img class="img-responsive img-portfolio img-hover" src="{{service.items.p6Source}}" alt="">
                 </a>
             </div>
         </div>
@@ -232,33 +100,14 @@ import { HostService } from './host.service';
         <div class="row">
             <div class="col-lg-12">
                 <h2 class="page-header">
-                    <input
-                        class="form-control"
-                        type="text"
-                        placeholder="Feature Title"
-                        [ngModel]="input.ftitle"
-                        (keyup)="onftitleChange($event)"
-                    />
+                    {{service.items.ftitle}}
                 </h2>
             </div>
             <div class="col-md-6">
-                <textarea
-                    class="form-control"
-                    rows="14"
-                    placeholder="Feature contents"
-                    [ngModel]="input.contents"
-                    (keyup)="onContentsChange($event)"
-                ></textarea>
+                {{service.items.contents}}
             </div>
             <div class="col-md-6">
-                <img class="img-responsive" src="http://placehold.it/700x450" alt="">
-                    <input
-                        class="form-control"
-                        type="text"
-                        placeholder="Picture Source URL"
-                        [ngModel]="input.fpSource"
-                        (keyup)="onfpSourceChange($event)"
-                    />
+                <img class="img-responsive" src="{{service.items.fpSource}}" alt="">
             </div>
         </div>
         <!-- /.row -->
@@ -269,30 +118,11 @@ import { HostService } from './host.service';
         <div class="well">
             <div class="row">
                 <div class="col-md-8">
-                    <textarea
-                        class="form-control"
-                        rows="2"
-                        placeholder="Call to Action Content"
-                        [ngModel]="input.CTAcontents"
-                        (keyup)="onCTAcontentsChange($event)"
-                    ></textarea>
+                    {{service.items.CTAcontents}}
                 </div>
                 <div class="col-md-4">
-                    <a class="btn btn-lg btn-default btn-block" href="#">
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Button Contents"
-                            [ngModel]="input.BBcontents"
-                            (keyup)="onBBcontentsChange($event)"
-                        />
-                        <input
-                            class="form-control"
-                            type="text"
-                            placeholder="Link URL"
-                            [ngModel]="input.BBLink"
-                            (keyup)="onBBLinkChange($event)"
-                        />
+                    <a class="btn btn-lg btn-default btn-block" href="{{service.items.BBLink}}">
+                        {{service.items.BBcontents}}
                     </a>
                 </div>
             </div>
@@ -303,13 +133,7 @@ import { HostService } from './host.service';
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <input
-                        class="form-control"
-                        type="text"
-                        placeholder="Trademark"
-                        [ngModel]="input.TMcontents"
-                        (keyup)="onTMcontentsChange($event)"
-                    />
+                    {{service.items.TMcontent}}
                 </div>
             </div>
         </footer>
